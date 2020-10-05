c4932da325a74d6ee3b78606abb71e41e561a92d
cenab <batu@LAPTOP-OB1QVPMQ.localdomain>
2020-10-05 20:31:45 +0300

    batu part2 color part commit

--- app/src/main/java/com/example/team07/Shape.java
+++ app/src/main/java/com/example/team07/Shape.java
@@ -3,7 +3,7 @@ package com.example.team07;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "lime green";
+    private String color = "red";
     
     public Shape(int x, int y) {
         this.x = x;
