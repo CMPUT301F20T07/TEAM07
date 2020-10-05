5e7485b6cf9157ecfae7d081df240a4d7aeefcc7
Zoey Pu <jpu1@ualberta.ca>
2020-10-05 10:27:21 -0600

    part 2 edit Shape

--- app/src/main/java/com/example/team07/Shape.java
+++ app/src/main/java/com/example/team07/Shape.java
@@ -4,9 +4,39 @@ public abstract class Shape {
 
     private int x;
     private int y;
+    private String color = "blue";
+
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
