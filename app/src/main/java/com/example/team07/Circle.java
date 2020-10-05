f2b96672907c389a5630613593cbe807851e6197
Loading <wfymiss@163.com>
2020-10-05 23:07:17 +0800

    xingjiehe commit

---
+++ app/src/main/java/com/example/team07/Circle.java
@@ -0,0 +1,8 @@
+package com.example.team07;
+
+class Circle extends Shape {
+    private String color = "blue";
+    public Circle(int x, int y) {
+        super(x, y);
+    }
+}
--- app/src/main/java/com/example/team07/Shape.java
+++ app/src/main/java/com/example/team07/Shape.java
@@ -1,7 +1,6 @@
 package com.example.team07;
 
 public abstract class Shape {
-
     private int x;
     private int y;
 
